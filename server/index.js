e06adf4996301db5b4ce844c079cb85848703043
keosophon <keosophon8888@gmail.com>
2024-06-19 12:37:46 +1200

    connect mongoose

--- server/index.js
+++ server/index.js
@@ -1,5 +1,20 @@
+// Load environment variables from .env file
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+
+// Connect to MongoDB using Mongoose
+mongoose
+  .connect(process.env.URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
